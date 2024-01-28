e762ec590cafa661d760300bfccd4aeb73be3f51
Devi-CodingTime <devipurnima26899@gmail.com>
2024-01-28 21:03:39 +0530

    deleted

--- src/app.js
+++ src/app.js
@@ -11,6 +11,35 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 //Write DELETE endpoint for deleting the details of user
+app.delete("/api/v1/details/:id",(req,res)=>{
+  const id =  req.params.id;
+
+  const deletedUsers = userDetails.find(
+    (user) => user.id == id
+  );
+  
+  if(!deletedUsers)
+  {
+    res.status(404).send({status: "failed", message: "User not found!" })
+  }
+  const index = userDetails.indexOf(deletedUsers);
+
+  userDetails.splice(index,1);
+  fs.writeFile(`${__dirname}/data/userDetails.json`,
+    JSON.stringify(userDetails),
+    (err) =>{res.status(200).send({
+      status: "success", 
+
+      message: "User details deleted successfully",
+
+      data:{
+        details:deletedUsers
+      }
+      
+    })
+  }
+  )
+})
 
 // PATCH endpoint for editing user details
 app.patch("/api/v1/details/:id", (req, res) => {
